cb392910cff4a3961b153d58e6687508c3dd7041
Leschonander <33600597+Leschonander@users.noreply.github.com>
2020-04-09 13:16:42 -0400

    Add files via upload

---
+++ svelte-d3/README.md
@@ -0,0 +1,5 @@
+Based on this: https://github.com/jlengstorf/holy-buckets
+
+Turns out Svelte works just as well, and is likely easier to work with when wanting to do SVG related work.
+
+Viz works, just need to make the svg nicer...
---
+++ svelte-d3/src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+	import { onMount } from 'svelte';
+	import * as d3 from 'd3';
+	import Viz from './Viz.svelte'
+
+	var data = [30, 86, 168, 281, 303, 365];
+	
+	let bespokeData = [
+		{
+			donation_count: 10,
+			total_donated: 250,
+			party: "Democrat"
+		},
+		{
+			donation_count: 5,
+			total_donated: 125,
+			party: "Republican"
+		}
+	]
+	console.log(bespokeData)
+
+
+</script>
+
+<style>
+
+</style>
+<Viz />
+
+<div ></div>
